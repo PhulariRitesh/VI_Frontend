ad4159e1efde505a5d3de2c03ed09b5229118339
Ritesh Prakashrao Phulari <riteshprakashraop@gmail.com>
2026-01-13 15:49:48 +0530

    Fetched producvt data and displayed it using The table

--- virtual_intern/src/App.tsx
+++ virtual_intern/src/App.tsx
@@ -1,34 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [products, setProducts] = useState([]);
 
+  useEffect(() => {
+    const fetchProducts = async () => {
+      return new Promise(async (resolve) => {
+        const response = await fetch('https://dummyjson.com/products');
+        const data = await response.json();
+
+        setTimeout(() => resolve(data), 1000);
+      }).then((data: any) => {
+        console.log(data);
+        setProducts(data.products);
+      });
+    }
+
+    fetchProducts();
+  },[]);
+
+  if (products.length === 0) {
+    return <div>Loading...</div>; 
+  }
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <table>
+      <thead>
+        <tr>
+        <th>Title</th>
+        <th>Brand</th>
+        <th>Category</th>
+        <th>Price</th>
+        <th>Rating</th>
+      </tr>
+      </thead>
+      <tbody>
+        {products.map((product) => (
+          <tr key={product.id}>
+          <td>{product.title}</td>
+          <td>{product.brand}</td>
+          <td>{product.category}</td>
+          <td>{product.price}</td>
+          <td>{product.rating}</td>
+        </tr>
+        ))
+      }
+      </tbody>
+      </table>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+        </>
   )
 }
 
